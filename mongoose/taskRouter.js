5dd88cba2163e41a7cb73915514b43277ef1a1b7
Andrey Borue <andrey.borue@gmail.com>
2018-06-15 19:32:59 +0300

    Mongoose 02

--- mongoose/taskRouter.js
+++ mongoose/taskRouter.js
@@ -17,36 +17,102 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({"extended": true}));
 
 router.get('/', (request, response) => {
-  Task.find({}).lean().exec((err, docs) => {
-    if (err) throw err;
+  let query = {};
+  let or = [];
+
+  if (request.query.title) {
+    or.push({title: request.query.title});
+  }
+
+  if (request.query.description) {
+    or.push({description: request.query.description});
+  }
+
+  if (or.length) {
+    query = {$or : or};
+  }
+
+  Task.find(query).lean().exec((err, docs) => {
+    if (err) {
+      response.status(400);
+      response.write(err);
+      response.send();
+      return;
+    }
     response.json(docs);
   });
 });
 
 router.post('/', (request, response) => {
   (new Task(request.body)).save((err) => {
-    if (err) throw err;
+    if (err) {
+      response.status(400);
+      response.write(err);
+      response.send();
+      return;
+    }
     response.json({'result': 'saved'});
   });
 });
 
 router.post('/:id/:status', (request, response) => {
   Task.update({_id: request.params.id}, {"status": request.params.status}, {overwrite: false}, (err) => {
-    if (err) throw err;
+    if (err) {
+      response.status(400);
+      response.write(err);
+      response.send();
+      return;
+    }
     response.json({'result': 'updated', 'status': request.params.status});
   });
 });
 
 router.put('/:id', (request, response) => {
-  Task.update({_id: request.params.id}, request.body, {overwrite: true}, (err) => {
-    if (err) throw err;
-    response.json({'result': 'updated'});
+  Task.findOne({_id: request.params.id}, (err, doc) => {
+    if (err) {
+      response.status(404);
+      response.json({'error': 'not found'});
+      response.send();
+      return;
+    }
+
+    if (request.body.title) {
+      doc.title = request.body.title;
+    }
+
+    if (request.body.description) {
+      doc.description = request.body.description;
+    }
+
+    if (request.body.status) {
+      doc.status = request.body.status;
+    }
+
+    if (request.body.user) {
+      doc.user = request.body.user;
+    }
+
+    doc.save((err) => {
+      if (err) {
+        response.status(400);
+        response.write(err);
+        response.send();
+        return;
+      }
+
+      response.json({'result': 'updated'});
+
+    });
   });
 });
 
 router.delete('/:id', (request, response) => {
   Task.remove({_id: request.params.id}, (err) => {
-    if (err) throw err;
+    if (err) {
+      response.status(400);
+      response.write(err);
+      response.send();
+    }
     response.json({'result': 'deleted'});
   });
 });
